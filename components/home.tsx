ceb6fd4d5765abe17217514bbaeb14257e3980fb
akash.bawa1 <bawa2027@gmail.com>
2022-10-05 22:39:33 +0530

    update

--- components/home.tsx
+++ components/home.tsx
@@ -5,7 +5,7 @@ const Home : NextPage = () => {
         <div className="home-page">
             <div className="description">
                 <h1>Hello I am Akash Bawa</h1>
-                <h6> I am a full stack developer based in the India</h6>
+                <h6> I am a full stack developer based in th e India</h6>
             </div>
             <div className="image">
 
