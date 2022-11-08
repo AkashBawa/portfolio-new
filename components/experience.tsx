af01b7cc6512e67ec8ca92c3835a64c768c102fe
Akash Bawa <bawa2027@gmail.com>
2022-11-08 15:34:47 +0530

    [skills]: complete

--- components/experience.tsx
+++ components/experience.tsx
@@ -3,7 +3,14 @@ import { NextPage } from "next";
 const Experience : NextPage = () => {
     return (
         <div className="experience-page">
-            Experience page
+            <div className="heading">
+                <h1> Experience</h1>
+                <span>Here is list of my hard earned experience. I am always keen to learn new things and  sometime even end up over worked.</span>
+            </div>
+            <div className="experience-div">
+                <div className="experience-list"></div>
+                <div className="experience-list"></div>
+            </div>
         </div>
     )
 }
--- components/skills.tsx
+++ components/skills.tsx
@@ -41,11 +41,22 @@ const Skills : NextPage = () => {
             logo:  { url:  '/angular.png', backgroungColor: 'white' }
         },
         {
+            name: 'ReactJS',
+            startDate: 'April 2019',
+            backgroundColor: {
+                light : '#5ED3F3',
+                dark:  '#046077'
+            },
+            textColor: 'white',
+            projects : ['Outgrow', 'Healthcrum'],
+            logo:  { url:  '/react.png', backgroungColor: 'white' }
+        },
+        {
             name: 'NodeJS',
             startDate: 'April 2019',
             backgroundColor: {
                 light : '#5FAE45',
-                dark:  '#3E843D'
+                dark:  '#1a6803'
             },
             textColor: 'white',
             projects : ['Outgrow', 'Healthcrum'],
@@ -55,19 +66,68 @@ const Skills : NextPage = () => {
             name: 'Javascript',
             startDate: 'April 2019',
             backgroundColor: {
-                light : '#c6b637',
-                dark:  '#847501'
+                light : '#f7e156',
+                dark:  '#b79f00'
             },
             textColor: 'white',
             projects : ['Outgrow', 'Healthcrum'],
             logo:  { url:  '/javascript.png', backgroungColor: 'white' }
         },
+        {
+            name: 'ExpressJS',
+            startDate: 'April 2019',
+            backgroundColor: {
+                light : '#4f4848',
+                dark:  '#0B0B0B'
+            },
+            textColor: 'white',
+            projects : ['Outgrow', 'Healthcrum'],
+            logo:  { url:  '/express.png', backgroungColor: 'white' }
+        },
+        {
+            name: 'Socket.io',
+            startDate: 'April 2019',
+            backgroundColor: {
+                light : '#f76ac6',
+                dark:  '#8e0475'
+            },
+            textColor: 'white',
+            projects : ['Outgrow', 'Healthcrum'],
+            logo:  { url:  '/socketio.png', backgroungColor: 'white' }
+        },
+        {
+            name: 'AWS Lambda',
+            startDate: 'April 2019',
+            backgroundColor: {
+                light : '#F37C1B',
+                dark:  '#964c13'
+            },
+            textColor: 'white',
+            projects : ['Outgrow', 'Healthcrum'],
+            logo:  { url:  '/lambda.png', backgroungColor: 'white' }
+        },
+        {
+            name: 'HTML',
+            startDate: 'April 2019',
+            backgroundColor: {
+                light : '#E54C21',
+                dark:  '#962506'
+            },
+            textColor: 'white',
+            projects : ['Outgrow', 'Healthcrum'],
+            logo:  { url:  '/html.png', backgroungColor: 'white' }
+        },
+
+        
     ]
 
     return (
         <div className="skills-wrapper">
             <div className="section">
-                <div className="heading"> My skills</div>
+                <div className="heading">
+                    <h1> My skills </h1>
+                    <span>Here is list of my hard earned skills. I am always keen to learn new things and  sometime even end up over worked.</span>
+                </div>
                 <div className="list-skills">
                     {
                         skills.map((skill) => {
