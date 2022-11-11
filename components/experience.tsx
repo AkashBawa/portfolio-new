d3fedde5a796eeaa0a207ca276e96b72a5f56ac0
Akash Bawa <bawa2027@gmail.com>
2022-11-11 15:13:35 +0530

    [experience]: init

--- components/experience.tsx
+++ components/experience.tsx
@@ -2,16 +2,36 @@ import { NextPage } from "next";
 
 interface ExperienceI {
     companyName: string,
+    designation: string,
     location: string,
     startDate: string,
     endDate: string,
     projects: string[],
     logo: string,
     credentials: string,
-    description: string,
+    compUrl : string,
+    aboutCompany ?: string,
+    roles : string[]
 }
 
 const Experience : NextPage = () => {
+
+    const experiences : ExperienceI[] = [
+        {
+            companyName: 'Venturepact',
+            designation: 'Software developer',
+            location: 'Jalandhar',
+            startDate: 'June 2021',
+            endDate: 'Dec 2022',
+            projects: ['Outgrow', 'Omniengage'],
+            logo : '',
+            credentials: '',
+            compUrl: 'www.venturepact.com',
+            aboutCompany: '',
+            roles: ['Handle this', 'handle that']
+        }
+    ]
+
     return (
         <div className="experience-page">
             <div className="section-heading">
@@ -19,8 +39,20 @@ const Experience : NextPage = () => {
                 <span>Here is list of my hard earned experience. I am always keen to learn new things and  sometime even end up over worked.</span>
             </div>
             <div className="experience-div">
+                {
+                    experiences.map((experience, index) => {
+                        return (
+                            <div className="experience-list">
+                                <h2>Software developer</h2>
+                                <span className="description"> {`${experience.companyName} | ${experience.compUrl} | ${experience.startDate}-${experience.endDate}`} </span>
+                            </div>
+                        )
+                    })
+                }
+                
                 <div className="experience-list">
-                    <h2>Venturepact</h2>
+                    <h2>Software developer</h2>
+                  
                 </div>
             </div>
         </div>
