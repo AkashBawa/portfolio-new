ee0205e0a238472174df5b7f6af20369c8bbcc7c
Akash Bawa <bawa2027@gmail.com>
2022-11-15 11:39:56 +0530

    experience addes

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-
+import Image from 'next/image'
 interface ExperienceI {
     companyName: string,
     designation: string,
@@ -24,12 +24,38 @@ const Experience : NextPage = () => {
             startDate: 'June 2021',
             endDate: 'Dec 2022',
             projects: ['Outgrow', 'Omniengage'],
-            logo : '',
+            logo : '/venturepact.png',
             credentials: '',
             compUrl: 'www.venturepact.com',
             aboutCompany: '',
-            roles: ['Handle this', 'handle that']
+            roles: ['Sitewide Development in Frontend(angular2) + Backend (MongoDB and Nodejs)', "Managing Outgrow's different development environments (Dev - Staging - Production)", "Defining development process and execution."]
+        },
+        {
+            companyName: 'Dots',
+            designation: 'MEAN Stack Intern',
+            location: 'Remote',
+            startDate: 'Jan 2021',
+            endDate: 'May 2021',
+            projects: ['Healthcrum'],
+            logo : '/dots.png',
+            credentials: '',
+            compUrl: 'www.dotsforall.com',
+            aboutCompany: '',
+            roles: ['Worked on the Web sockets , Angular 8 , Node.js, MongoDB and Express', "Worked on project HealthCrum.in using MEAN stack."]
         }
+        // {
+        //     companyName: 'Impinge Solution',
+        //     designation: 'MEAN Stack Intern',
+        //     location: 'Mohali',
+        //     startDate: 'June 2019',
+        //     endDate: 'July 2019',
+        //     projects: ['Tourism'],
+        //     logo : '/impinge.jpeg',
+        //     credentials: '',
+        //     compUrl: 'https://www.impingesolutions.com/',
+        //     aboutCompany: '',
+        //     roles: ['Worked on the Web sockets , Angular 8 , Node.js, MongoDB and Express', "Worked on project HealthCrum.in using MEAN stack."]
+        // }
     ]
 
     return (
@@ -43,17 +69,27 @@ const Experience : NextPage = () => {
                     experiences.map((experience, index) => {
                         return (
                             <div className="experience-list" key={`exp-${index}`}>
-                                <h2>Software developer</h2>
+                                <h2> {experience.designation} 
+                                    <span>
+                                        <Image src={experience.logo} height={30} width={30}></Image>
+                                    </span>
+                                </h2>
                                 <span className="description"> {`${experience.companyName} | ${experience.compUrl} | ${experience.startDate}-${experience.endDate}`} </span>
+                                <div className="roles">
+                                    <ol>
+                                        {
+                                            experience.roles.map((role, index) => {
+                                                return (
+                                                    <li key={`role-${index}`}>{role}</li>
+                                                )
+                                            })
+                                        }
+                                    </ol>
+                                </div>
                             </div>
                         )
                     })
                 }
-                
-                <div className="experience-list">
-                    <h2>Software developer</h2>
-                  
-                </div>
             </div>
         </div>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,10 @@ import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <div> 
+      <Head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous"/>
+      </Head>
+     
       <Component {...pageProps} />
     </div>
 }
